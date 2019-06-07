0edc108d98c106dc733d7d25f9bb4b5d3fd271c5
Samrat Saurav Jaiswal <39924445+SamratSauravJaiswal@users.noreply.github.com>
2019-06-07 11:21:08 +0530

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-#fast2sms
-username = 'saurav.kumar'
-password = 'rj12345'
-api = "tMOCa3qY8DkTzFfiLNQyBolgH65v2IUARX0e17urcKZjnS4pmVOLklQbV7eCd2rjPMi9Run5ts6NFUa3"
-
-#twilio
-account = "AC8e1ca42457a133d8d7be25ba0d376a47"
-token = "29c3281199b18d526d207056b381bb09"
